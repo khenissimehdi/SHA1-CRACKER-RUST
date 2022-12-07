c3419276fd3fb7128e64ddf25aef12cccdaf6ae2
mehdi <mehdi@MacBook-Pro-de-mehdi.local>
2022-12-07 21:41:16 +0100

    hash cracker finisheed

--- sha1_cracker/src/main.rs
+++ sha1_cracker/src/main.rs
@@ -1,3 +1,4 @@
+use sha1::Digest;
 use std:: {
     error::Error,
     env, fs::File, io::{BufReader, BufRead}
@@ -18,13 +19,25 @@ fn main() -> Result<(), Box<dyn Error>>  {
         return Err("sha1 hash is not valid".into());
     }
 
-    let wordlistFile = File::open(&args[1])?;
-    let reader = BufReader::new(&wordlistFile);
-    
-    for line in reader.lines()  {
-        let line = line?.trim().to_string();
-        println!("{}",line);
+
+    let wordlist_file = File::open(&args[1])?;
+    let reader = BufReader::new(&wordlist_file);
+
+    let hash_to_crack = args[2].trim();
+    if hash_to_crack.len() != SHA1_HEX_STRING_LENGTH {
+        return Err("sha1 hash is not valid".into());
     }
+
+    for line in reader.lines()  {
+        let line = line?;
+        let common_password = line.trim().to_string();
+        if hash_to_crack == &hex::encode(sha1::Sha1::digest(common_password.as_bytes())) {
+            println!("Password found : {}",&common_password);
+            return Ok(());
+        }
+    }
+
+    println!("Password wasn't found in the wordlist");
     Ok(())
 
 }
